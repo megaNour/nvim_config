22b950094cf5bc5c040e132eb61e395f92f63a31
megaNour <alkotobnour@gmail.com>
2026-01-08 17:23:31 +0100

    format

--- lua/plugins/colors_tokyonight.lua
+++ lua/plugins/colors_tokyonight.lua
@@ -1,10 +1,13 @@
-return { 
-    'folke/tokyonight.nvim',
-    priority = 1000, 
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {}
-
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  }
+return {
+  "folke/tokyonight.nvim",
+  priority = 1000,
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require("tokyonight").setup({
+      styles = {
+        comments = { italic = false }, -- Disable italics in comments
+      },
+    })
+    vim.cmd.colorscheme("tokyonight-night")
+  end,
+}
